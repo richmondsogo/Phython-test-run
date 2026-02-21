b085cf8d40340dd8080175d554ed89019de3dd6a
richmondsogo <richmondsogo@gmail.com>
2026-02-21 16:58:14 +0100

    feat: implement password generation and translation functionality

--- passwordchecker/checkmypass.py
+++ passwordchecker/checkmypass.py
@@ -93,5 +93,5 @@ def main(args):
             print(f"{password} was not found Your password is secure")
         return "done"
 
-
-main(sys.argv[1:])
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
