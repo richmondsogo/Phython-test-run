4a780b618b17e2e87f1d8d630cf8d535273414fd
richmondsogo <richmondsogo@gmail.com>
2026-03-31 13:23:23 +0100

    removed json web token

--- django-react/backend/backend/urls.py
+++ django-react/backend/backend/urls.py
@@ -12,5 +12,3 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
 ]
 
-#  "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc3NTA0MzE3NiwiaWF0IjoxNzc0OTU2Nzc2LCJqdGkiOiIyNTM5ZmM0NjM5Nzk0ZDhmYWU2NGQ4MTgyMmJhMDQ4NiIsInVzZXJfaWQiOiIxIn0.gxfhCg_kuQA1qQH1diucDPhenf50y5aJEQQhLj1Az4w",
-#     "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc0OTU4NTc2LCJpYXQiOjE3NzQ5NTY3NzYsImp0aSI6IjcxODI1ZTlmYTgyMjRjMDliYTg3NzAzNmY1YTA3OGYzIiwidXNlcl9pZCI6IjEifQ.kOImfj0OKZ-hz87vdSdQ1t_3pMBiouWS0gm3ynMZ4XA"
