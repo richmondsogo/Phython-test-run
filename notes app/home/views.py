847e2298afda62cc6c7ac0af6e538cb6bf57b78b
richmondsogo <richmondsogo@gmail.com>
2026-04-16 14:17:07 +0100

    feat: implement home and authorizer views with corresponding templates and URL routing

--- notes app/home/views.py
+++ notes app/home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def home(request):
-    return render(request, "home/home.html")
+    return render(request, 'home/welcome.html', {"today" : datetime.today()})
+
+
+@login_required(login_url='/admin')
+def authorizer(request):
+    return render(request, 'home/authorized.html', {})
