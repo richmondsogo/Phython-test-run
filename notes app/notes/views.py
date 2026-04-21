4ca59e54ad4063257f28c843c4170624257282bb
richmondsogo <richmondsogo@gmail.com>
2026-04-21 14:53:26 +0100

    feat: add notes app with views, templates, and URL routing

--- notes app/notes/views.py
+++ notes app/notes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import Http404
+from . models import Notes  
 
-# Create your views here.
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+def details(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        return render(request, 'notes/note_not_found.html', {'note_id': pk})   
+    return render(request, 'notes/notes_details.html', {'note': note})
