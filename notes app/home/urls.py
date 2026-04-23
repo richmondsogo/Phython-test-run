d6cd97a87bed3359086125bc6b2529c494f4631b
richmondsogo <richmondsogo@gmail.com>
2026-04-23 15:47:48 +0100

    feat: update URL patterns to remove name attributes and add static files configuration

--- notes app/home/urls.py
+++ notes app/home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.HomeView.as_view(), name='home'),
-    path('authorizer', views.AuthorizedView.as_view(), name='authorizer'),
+    path('home', views.HomeView.as_view()),
+    path('authorizer', views.AuthorizedView.as_view(),),
 ]
